63784e271296cd0dd4706ef01d91f48a8c326180
thelastgrim <48408618+thelastgrim@users.noreply.github.com>
2019-06-27 17:55:42 +0200

    Main klasa

---
+++ WebShop_Tim4/src/model/Main.java
@@ -0,0 +1,19 @@
+package model;
+
+import gui.AppWindow;
+
+
+public class Main {
+	
+	private Aplikacija aplikacija;
+
+	public static void main(String[] args) {
+		
+			
+		AppWindow window = new AppWindow();
+		window.frame.setVisible(true);
+		
+		
+	}
+
+}
